e6cf970d6de7f8359e0739d7519f643c36a2d8c4
svinkaKar <svinkakarkar@gmail.com>
2025-01-28 21:10:48 +0200

    step 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,3 +23,7 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
---
+++ app/src/main/java/hexlet.code/Cli.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+
+    public static void helloAndGetName(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!\nMay I have your name?");
+        String nameOfUser = sc.nextLine();
+        System.out.println(String.format("Hello, %s!", nameOfUser));
+    }
+}
